d3b524360fd4c9648f87ad3e2de918e3813a057d
yaojiejia <yaojiejia0715@gmail.com>
2023-04-29 16:12:57 -0700

    latest

--- album/src/main/java/com/example/album/LoginController.java
+++ album/src/main/java/com/example/album/LoginController.java
@@ -6,11 +6,17 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginController {
+    @FXML
+    private TextField loginusername;
+    @FXML
+    private TextField loginpassword;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -21,4 +27,47 @@ public class LoginController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void login(){
+        String url = "jdbc:mysql://localhost:3306/myDB";
+        String username = "root";
+        String password = "jiayaojie0715";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+            String verified = " ";
+            while(resultSet.next()){
+                if(resultSet.getString(2 ).equals(loginusername.getText())){
+                    verified = loginusername.getText();
+                }
+            }
+
+            if(verified.equals(loginusername.getText())) {
+                String sql = "SELECT password FROM users WHERE user_name = ?";
+                PreparedStatement preparedStmt = connection.prepareStatement(sql);
+                preparedStmt.setString(1, loginusername.getText());
+
+
+            }
+            connection.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public void goToUI(String u, String p){
+
+    }
+
+
 }
--- album/src/main/java/com/example/album/SignupController.java
+++ album/src/main/java/com/example/album/SignupController.java
@@ -6,11 +6,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class SignupController {
+    @FXML
+    private TextField signupusername;
+    @FXML
+    private TextField signuppassword;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -20,5 +27,35 @@ public class SignupController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    public void clickToSignup(){
+        String url = "jdbc:mysql://localhost:3306/myDB";
+        String username = "root";
+        String password = "jiayaojie0715";
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+
+            String sql = " insert into users (id, user_name, password)"
+                    + " values (?, ?, ?)";
+            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+            preparedStmt.setInt (1, (int)((Math.random()*(100-0))+0));
+
+            preparedStmt.setString (2, signupusername.getText());
+            preparedStmt.setString   (3, signuppassword.getText());
+            preparedStmt.execute();
+
+
+
+            connection.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
